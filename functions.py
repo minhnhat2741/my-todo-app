de5d5e27ceacda1b1dbcae588f26ed8b992a0c7a
cu274 <minhnhat2741@gmail.com>
2026-01-10 16:18:33 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """
+    Read a text file and return
+    the list of files
+    """
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos( todos_arg, filepath=FILEPATH):
+    """
+    Read a text file and write to the file
+    the list of files
+    """
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("hello world")
+
