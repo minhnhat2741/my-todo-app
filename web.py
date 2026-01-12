428bce515f231a4850b2bc44260cf1e1477e5fd9
cu274 <minhnhat2741@gmail.com>
2026-01-13 00:25:17 +0100

    Upgraded version 1

--- web.py
+++ web.py
@@ -1,32 +1,38 @@
 import streamlit as st
 import functions
+import hashlib
+
 
 st.title("Apotek assistent")
 st.subheader("Ting å gjøre på apotek")
 st.write("""Appen som hjelper deg å ungå hodepine""")
 
-#todos = functions.get_todos()
+
 
 def add_todo():
     todo = st.session_state["new_todo"].strip()
     if not todo:
         return
+    todos = functions.get_todos()
     todos.append(todo + "\n")
-
     functions.write_todos(todos)
     st.session_state["new_todo"] = "" # clear input
 
 # always read fresh on each rerun
 todos = functions.get_todos()
 
+delete_line = None
 
-for index, todo in enumerate(todos):
-    clean = todo.strip()
-    checkbox = st.checkbox(clean, key=f"todo_{index}")
-    if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos)
 
-        st.rerun()
+for i, line in enumerate(todos):
+    text = line.strip()
+    key = hashlib.md5(f"{i}:{line}".encode()).hexdigest()
+    if st.checkbox(text, key=key):
+        delete_line = i
+
+if delete_line is not None:
+    todos.pop(delete_line)
+    functions.write_todos(todos)
+    st.rerun()
 
 st.text_input(label="", placeholder="Add something", on_change=add_todo, key="new_todo")
