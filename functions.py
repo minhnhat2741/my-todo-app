319c1133bc75d0291f7c324f55535941b1f381b4
cu274 <minhnhat2741@gmail.com>
2026-01-13 00:01:14 +0100

    Upgraded version

--- functions.py
+++ functions.py
@@ -1,19 +1,21 @@
-FILEPATH = "todos.txt"
+from pathlib import Path
+
+FILEPATH = Path("todos.txt")
 
 def get_todos(filepath=FILEPATH):
-    """
-    Read a text file and return
-    the list of files
-    """
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
+
+    """Read a text file and return a list of todos (lines) """
+    filepath = Path(filepath)
+    if not filepath.exists():
+        filepath.write_text('')
+        return []
+    #with open(filepath, "r") as file_local:
+    #    todos_local = file_local.readlines()
+
+    return filepath.read_text().splitlines(keepends=True)
 
 def write_todos( todos_arg, filepath=FILEPATH):
-    """
-    Read a text file and write to the file
-    the list of files
-    """
+    """ Write the list of todos (lines) to a text file """
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
 
--- web.py
+++ web.py
@@ -1,26 +1,32 @@
 import streamlit as st
 import functions
 
-todos = functions.get_todos()
+st.title("Apotek assistent")
+st.subheader("Ting å gjøre på apotek")
+st.write("""Appen som hjelper deg å ungå hodepine""")
+
+#todos = functions.get_todos()
 
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
+    todo = st.session_state["new_todo"].strip()
+    if not todo:
+        return
+    todos.append(todo + "\n")
+
     functions.write_todos(todos)
+    st.session_state["new_todo"] = "" # clear input
 
-
-
-st.title("My Todo App")
-st.subheader("This is my todo app")
-st.write("""This app is to increase your productivity""")
+# always read fresh on each rerun
+todos = functions.get_todos()
 
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    clean = todo.strip()
+    checkbox = st.checkbox(clean, key=f"todo_{index}")
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
+
         st.rerun()
 
 st.text_input(label="", placeholder="Add something", on_change=add_todo, key="new_todo")
